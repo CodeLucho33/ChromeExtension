0bab4335c5b8034e36e1fcc64df42541e250834e
CodeLucho33 <code.lucho33@gmail.com>
2024-07-14 10:19:27 -0500

    Try to solve error with tabBtn

--- script.js
+++ script.js
@@ -2,13 +2,50 @@ let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById("tab-btn")
 let listItems = ""
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+deleteBtn.addEventListener("dblclick", function(){
+localStorage.clear()
+myLeads =[]
+renderLeads()
+})
+
+// tabBtn.addEventListener("click", function(){
+//     // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     //     // since only one tab should be active and in the current window at once
+//     //     // the return variable should only have one entry
+//     //     let activeTab = tabs[0]
+//     //     var activeTabId = activeTab.id // or do whatever you need
+
+//     // })
+    
+//     myLeads.push(tabs[0].url)
+//     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+//     render(myLeads)
+    
+// })
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+
 inputBtn.addEventListener('click', function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
-
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
@@ -19,7 +56,6 @@ function renderLeads() {
                     ${myLeads[i]}
                 </a>
             </li>
-        `
-    }
+        ` }
     ulEl.innerHTML = listItems  
 }
